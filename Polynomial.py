b771da977cd49e6b1ad0ecb737f82b24a5815a83
Polyakov Igor <polykovio@mail.ru>
2018-03-19 17:04:52 +0300

    Fixed some bugs. Added tests

--- Polynomial.py
+++ Polynomial.py
@@ -15,34 +15,32 @@ class Polynomial:
     def __add__(self, rhValue):
         if isinstance(rhValue, Polynomial):
             if (self.degree == rhValue.degree):
-                result = Polynomial(list(map(operator.add, self.coeffs, rhValue.coeffs)))
-                result.prepocessingOfCoeffsList(result.coeffs)
-                return result
+                result = list(map(operator.add, self.coeffs, rhValue.coeffs))
+                return Polynomial(result)
             elif (self.degree > rhValue.degree):
                 rhPolynomialCoeffs = [0] * (self.degree - rhValue.degree) + rhValue.coeffs
-                result = Polynomial(list(map(operator.add, self.coeffs, rhPolynomialCoeffs)))
-                result.prepocessingOfCoeffsList(result.coeffs)
-                return result
+                result = list(map(operator.add, self.coeffs, rhPolynomialCoeffs))
+                return Polynomial(result)
             else:
                 lhPolynomialCoeffs = [0] * (rhValue.degree - self.degree) + self.coeffs
-                result = Polynomial(list(map(operator.add, lhPolynomialCoeffs, rhValue.coeffs)))
-                result.prepocessingOfCoeffsList(result.coeffs)
-                return result
+                result = list(map(operator.add, lhPolynomialCoeffs, rhValue.coeffs))
+                return Polynomial(result)
         else:
-            if not isinstance(coeff, (int, float)):
+            if not isinstance(rhValue, (int, float)):
                 raise Exception("Incorrect type of value. 'int' or 'float' type is expected.")
             result = Polynomial(self.coeffs)
             result.coeffs[len(result.coeffs) - 1] += rhValue
             return result
 
     def __mul__(self, rhValue):
-        if isinstance(rhPolynomial, Polynomial):
+        if isinstance(rhValue, Polynomial):
             result = [0] * (self.degree + rhValue.degree + 1)
             for i, lhCoeff in enumerate(self.coeffs):
                 for j, rhCoeff in enumerate(rhValue.coeffs):
-                    res[i + j] += lhCoeff * rhCoeff
+                    result[i + j] += lhCoeff * rhCoeff
+            return Polynomial(result)
         else:
-            if not isinstance(coeff, (int, float)):
+            if not isinstance(rhValue, (int, float)):
                 raise Exception("Incorrect type of value. 'int' or 'float' type is expected.")
             result = Polynomial([coef * rhValue for coef in self.coeffs])
             return result
@@ -52,27 +50,52 @@ class Polynomial:
             raise Exception("Incorrect type of input data. Polynomial is expected.")
         return self.coeffs == rhPolynomial.coeffs
 
+    def __ne__(self, rhPolynomial):
+        if not isinstance(rhPolynomial, Polynomial):
+            raise Exception("Incorrect type of input data. Polynomial is expected.")
+        return self.coeffs != rhPolynomial.coeffs
+
     def __str__(self):
         polynomialStr = ""
         coeffsCount = len(self.coeffs)
         if (coeffsCount == 1):
-            polynomialStr += self.coeffs[0]
+            polynomialStr += str(self.coeffs[0])
             return polynomialStr
         for i, coeff in enumerate(self.coeffs):
             if (((coeffsCount - 1) - i) == self.degree):
-                polynomialStr += coeff + 'x' + (self.degree)
+                if (self.degree == 1):
+                    degreeStr = ''
+                else:
+                    degreeStr = str((self.degree))
+                if (coeff == 1):
+                    coeffStr = ''
+                elif (coeff == -1):
+                    coeffStr = '-'
+                else:
+                    coeffStr = str(coeff)
+                polynomialStr += coeffStr + 'x' + degreeStr
             elif (((coeffsCount - 1) - i) == 0):
                 if (coeff > 0):
-                    polynomialStr += '+' + coeff
-                else:
-                    polynomialStr += coeff
+                    polynomialStr += '+' + str(coeff)
+                elif (coeff < 0):
+                    polynomialStr += str(coeff)
                 return polynomialStr
             else:
                 if (coeff != 0):
-                    if (coeff > 0):
-                        polynomialStr += '+' + coeff + 'x' + (degree - i)
+                    if (self.degree - i == 1):
+                        degreeStr = ''
                     else:
-                        polynomialStr += coeff + 'x' + (degree - i)
+                        degreeStr = str((self.degree - i))
+                    if (coeff == 1):
+                        coeffStr = ''
+                    elif (coeff == -1):
+                        coeffStr = '-'
+                    else:
+                        coeffStr = str(coeff)
+                    if (coeff > 0):
+                        polynomialStr += '+' + coeffStr + 'x' + degreeStr
+                    else:
+                        polynomialStr += coeffStr + 'x' + degreeStr
 
     def checkObjectsTypeCorrectness(self, coeffsList):
         for i, coeff in enumerate(coeffsList):
